3dc2795679f331d27463ee3b4fc4237d96612003
i-r-a <35972917+i-r-a@users.noreply.github.com>
2018-03-29 20:55:00 -0400

    Update thread.c

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -308,7 +308,8 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+    list_push_back (&ready_list, &cur->elem,(list_less_func *) &cmp_priority,
+			  NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
